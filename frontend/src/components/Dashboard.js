1c45df5b2427bd670fd2361b9e31cbe05d95ed4f
Md Aftab <mda552669@gmail.com>
2022-09-06 13:01:07 +0530

    boiler components added

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,12 @@
-import react from "react"
-
+import React from "react"
+import Footer from "./footer"
 const DashBoard = () => {
 
 
   return (
     <div>
       <h1>Dr. Pace</h1>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ frontend/src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../css/footer.css'
+const footer = () => {
+
+
+  return (<>
+
+    <div className="container">
+        <div className="items">Copyright @2022 All rights reserved</div>
+
+    </div>
+
+  </>)
+}
+
+export default footer;
